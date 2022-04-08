bb4c64494da489eb620b046d6d77eb6c633811c9
divops.network <92889629+divopsresources@users.noreply.github.com>
2022-04-08 11:04:15 -0700

    Update and rename add-threes.sh to install-import.sh

--- add-threes.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env import
-
-# The URL is downloaded once, cached forever, and then sourced
-import "https://git.io/fAWiz"
-
-$=add 3 3
-add $ 3 
-# 9
---
+++ install-import.sh
@@ -0,0 +1,2 @@
+curl -sfLS https://import.sh > /usr/local/bin/import
+chmod +x /usr/local/bin/import
